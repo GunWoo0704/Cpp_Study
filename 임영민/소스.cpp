4e73e18a4f9d9bfad2b8dabb814f07d95aa4038b
youngmin61 <dladudals61@gmail.com>
2024-06-03 21:45:43 +0900

    youngmin week

---
+++ 임영민/소스.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::string address;
+
+    // 사용자로부터 이름 입력 받기
+    std::cout << "이름을 입력하세요: ";
+    std::getline(std::cin, name);
+
+    // 사용자로부터 주소 입력 받기
+    std::cout << "주소를 입력하세요: ";
+    std::getline(std::cin, address);
+
+    // 입력된 이름과 주소 출력
+    std::cout << "\n입력된 정보:" << std::endl;
+    std::cout << "이름: " << name << std::endl;
+    std::cout << "주소: " << address << std::endl;
+
+    return 0;
+}
