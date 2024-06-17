af01b08b6194e27f40400bc63bccca77514143a8
pys1018 <goodgate1018@naver.com>
2024-06-17 21:56:01 +0900

    박윤서 아무거나

---
+++ 박윤서/seo.cpp
@@ -0,0 +1,27 @@
+/*#include<iostream>
+
+void temperature(int);
+
+int main()
+{	
+	using namespace std;
+
+	int Celsius;
+
+	cout << "¼·¾¾ ¿Âµµ¸¦ ÀÔ·ÂÇÏ°í Enter Å°¸¦ ´©¸£½Ê½Ã¿À: " ;
+	cin >> Celsius;
+	cout << "¼·¾¾ " << Celsius;
+	temperature(Celsius);
+
+
+
+	return 0;
+}
+
+void temperature(int n)
+{
+	using namespace std;
+	n = 1.8 * n + 32.0;
+	cout << "µµ´Â È­¾¾·Î " << n << "µµ ÀÔ´Ï´Ù.";
+	
+}*/
---
+++ 박윤서/yun.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<cmath>
+
+
+int main() 
+{
+	using namespace std;
+	
+	string name;
+	string adress;
+	cout << "이름을 입력하시오 : ";
+	cin >> name;
+	cout << "주소를 입력하시오 : ";
+	cin >> adress;
+	cout << "이름 : " << name << endl;
+	cout <<"주소 : " << adress;
+
+
+	/*int road;
+	double km;
+
+	cout << "입력하시오 : ";
+	cin >> road;
+	km = road * 1.60934;
+	cout << km << " 킬로미터이다.";*/
+	
+	 
+	/*int age;
+
+	cout << "Enter your age : ";
+	cin >> age;
+	age = age * 12;
+	cout << "이때 당신의 나이를 월수로 나타낼 경우 " << age << "입니다.";*/
+
+
+	/*int Celsius;
+	int Fahrenheit;
+
+	cout << "섭씨 온도를 입력하고 Enter 키를 누르십시오 : ";
+	cin >> Celsius;
+	Fahrenheit = 1.8 * Celsius + 32.0;
+	cout << "섭씨 " << Celsius << "도는 화씨로 " << Fahrenheit << "도입니다.";*/
+
+
+
+	return 0;
+}
+
